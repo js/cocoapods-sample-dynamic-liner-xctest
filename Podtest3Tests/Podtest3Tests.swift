595d954fbb0729f824f1ff700af46cd4a475eb10
Johan Sørensen <js@urbansharing.com>
2018-11-30 09:30:03 +0100

    Initial Commit

---
+++ Podtest3Tests/Podtest3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Podtest3Tests.swift
+//  Podtest3Tests
+//
+//  Created by Johan Sørensen on 30/11/2018.
+//  Copyright © 2018 Johan Sørensen. All rights reserved.
+//
+
+import XCTest
+@testable import Podtest3
+
+class Podtest3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
